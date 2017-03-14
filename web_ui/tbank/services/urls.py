9f5498084ff99417a296ec744d9d618eb09b70b6
maffsojah <maffsojah1@gmail.com>
2017-03-14 08:13:50 +0200

    inactive_user table with post function

--- web_ui/tbank/services/urls.py
+++ web_ui/tbank/services/urls.py
@@ -5,4 +5,5 @@ from services import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^active_users', views.active_users, name='active_users'),
+    url(r'^inactive_users', views.inactive_users, name='inactive_users'),
 ]
--- web_ui/tbank/services/views.py
+++ web_ui/tbank/services/views.py
@@ -33,5 +33,33 @@ def active_users(request):
         users.Service_Level = line[14]
         #users.save()
         parsedData.append(users)
-    return render(request, 'services/customers.html', {'line': parsedData})
+    return render(request, 'services/active_users.html', {'line': parsedData})
+    #data.close()
+
+
+def inactive_users(request):
+    parsedData = []
+    data = open('/home/maffsojah/Projects/HIT_400/capstone_project/web_ui/tbank/services/static/datasets/dummyTest.csv', 'r')
+    data.next()
+    for line in data:
+        line = line.split(',')
+        users = Services.objects.filter()
+        users.Customer_ID = line[0]
+        users.Name = line[1]
+        users.Gender = line[2]
+        users.Address = line[3]
+        users.Nationality = line[4]
+        users.Account_Type = line[5]
+        users.Age = line[6]
+        users.Education = line[7]
+        users.Employment = line[8]
+        users.Salary = line[9]
+        users.Employer_Stability = line[10]
+        users.Customer_Loyalty = line[11]
+        users.Balance = line[12]
+        users.Residential_Status = line[13]
+        #users.Service_Level = line[14]
+        #users.save()
+        parsedData.append(users)
+    return render(request, 'services/inactive_users.html', {'line': parsedData})
     #data.close()
