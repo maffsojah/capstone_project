a9639d5ab04b4fdf78ab6bd609f9d796a1ceb79b
maffsojah <maffsojah1@gmail.com>
2017-03-18 19:30:15 +0200

    new admin panel

---
+++ web_admin/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/maffsojah/Projects/HIT_400/capstone_project/web_admin/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
