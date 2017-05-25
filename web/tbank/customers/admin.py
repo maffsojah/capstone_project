c8b8b602e59818d223b8111318ddd8937aa9a933
maffsojah <maffsojah1@gmail.com>
2017-05-25 22:11:17 +0200

    allocation conditions

--- web/tbank/customers/admin.py
+++ web/tbank/customers/admin.py
@@ -15,6 +15,7 @@ import findspark
 findspark.init()
 import tempfile
 from pyspark.ml.classification import LogisticRegression, LogisticRegressionModel
+from pyspark.ml.classification import MultilayerPerceptronClassifier, MultilayerPerceptronClassificationModel
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 from pyspark.ml import Pipeline
 from pyspark.ml.feature import OneHotEncoder, StringIndexer, VectorAssembler
@@ -30,79 +31,14 @@ spark = SparkSession.builder\
     .getOrCreate()
 
 temp_path = pjoin("/home/maffsojah/Projects/HIT_400/capstone_project/web/tbank/spark-warehouse")
-#globs['temp_path'] = temp_path
+
 reg_path = temp_path + '/reg2'
 model_path = temp_path + '/reg_model2'
+# mlp_path = temp_path + "/mlp"
+# mlpmodel_path = temp_path + "/mlp_model"
 
 from . import models
 from models import Customer, Service
-
-#####  PLAN B ALLOCATION
-
-
-# def allocate_service(ModelAdmin, request, queryset):
-#     platinum_customers = []
-#     silver_customers = []
-#     gold_customers = []
-#     non_customers = []
-#     message = ''
-#
-#
-#
-#     for customer in queryset:
-#         qs = Customer.objects.all()
-#         df = qs.to_dataframe(fieldnames=["Age","Education", "Employment", "Employer_Stability", "Residential_Status", "Salary", "Customer_Loyalty", "Balance", "Service_Level"])
-#         #df = read_frame(qs)
-#
-#         df['Age'] = np.where((df['Age']== '60 +'), 0, (np.where((df['Age'] == '36 - 59'), 1, 2)))
-#         df['Education'] = np.where((df['Education']== 'Highschool and below'), 0,  1)
-#         df['Employment'] = np.where((df['Employment']== 'Student'), 0, (np.where((df['Employment'] == 'Contract'), 1, 0)))
-#         df['Employer_Stability'] = np.where((df['Employer_Stability']== 'Unstable'), 0,  1)
-#         df['Residential_Status'] = np.where((df['Residential_Status'] == 'Rented'), 0,  1)
-#         df['Salary'] = np.where(((df['Salary'] >= 150) & (df['Salary'] <= 1000)), 0, (np.where(((df['Salary'] > 1000) & (df['Salary'] <= 10000)), 1, 2)))
-#         df['Customer_Loyalty'] = np.where((df['Customer_Loyalty'] <= 3), 0,  1)
-#         df['Balance'] = np.where(((df['Balance'] >= 150) & (df['Balance'] <= 2500)), 0, (np.where(((df['Balance'] > 2500) & (df['Balance'] <= 10000)), 1, 2)))
-#
-#         added = (df['Age'])+(df['Education'])+(df['Employment'])+(df['Salary'])+(df['Employer_Stability'])+(df['Customer_Loyalty'])+(df['Balance'])+(df['Residential_Status'])
-#
-#         silver = np.where( added <= 11)
-#
-#         gold = np.where(( added > 11) & (added <= 15))
-#
-#         platinum = np.where((added > 15) & (added <= 21))
-#
-#
-#         print (platinum)
-#         print(silver)
-#         print(gold)
-#
-#         if silver:
-#             customer.Service_Level = Service.objects.get(service_name = 'Silver Package')
-#             silver_customers.append(customer.Name)
-#         elif gold:
-#             customer.Service_Level = Service.objects.get(service_name = 'Gold Package')
-#             gold_customers.append(customer.Name)
-#         elif platinum:
-#             customer.Service_Level = Service.objects.get(service_name = 'Platinum Package')
-#             platinum_customers.append(customer.Name)
-#         else:
-#             customer.Service_Level = Service.objects.get(service_name = "No Service Package")
-#             non_customers.append(customer.name)
-#
-#         # else:
-#         #      message = "This customer does not meet the required criteria"
-#         customer.save()
-#
-#         if platinum_customers:
-#             message = 'The following customers are now Platinum Customers: {}'.format(', '.join(platinum_customers))
-#         if silver_customers:
-#             message = 'The following customers are now Silver Customers: {}'.format(', '.join(silver_customers))
-#         if gold_customers:
-#             message = 'The following customers are now Gold Customers: {}'.format(', '.join(gold_customers))
-#         if not platinum_customers and not silver_customers and not gold_customers:
-#             message = 'No customer changes made!'
-#         ModelAdmin.message_user(request, message, level=SUCCESS)
-# allocate_service.short_description = 'Allocate Service'
 
 
 """
@@ -159,6 +95,11 @@ def update_service(ModelAdmin, request, queryset):
         regModel2 = LogisticRegressionModel.load(model_path)
         #predict = regModel2.transform(predictSet)
         predict = regModel2.transform(predictSet).head().prediction
+
+        # ## {Prediction with Perceptron
+        # mlp2 = MultilayerPerceptronClassifier.load(mlp_path)
+        # mlpmodel2 = MultilayerPerceptronClassificationModel.load(mlpmodel_path)
+        #predict = mlpmodel2.transform(predictSet).head().prediction
         #predict.select("prediction", "label", "features").show()
         print(predict)
 
@@ -186,30 +127,16 @@ def update_service(ModelAdmin, request, queryset):
     if not silver_customers and not gold_customers and not platinum_customers:
         message = 'No customer changes have been made!'
     ModelAdmin.message_user(request, message, level=SUCCESS)
-update_service.short_description = 'Update'
+update_service.short_description = 'Update Service'
 spark.catalog.dropTempView("cust_temp")
 
-# This class allows function addition, multiplicationn, division etc.
-class operable:
-    def __init__(self, f):
-        self.f = f
-    def __call__(self, x):
-        return self.f(x)
 
-def op_to_function_op(op):
-    def function_op(self, operand):
-        def f(x):
-            return op(self(x), operand(x))
-        return operable(f)
-    return function_op
 
-for name, op in [(name, getattr(operator, name)) for name in dir(operator) if "__" in name]:
-    try:
-        op(1,2)
-    except TypeError:
-        pass
-    else:
-        setattr(operable, name, op_to_function_op(op))
+"""
+    RECOMENDATION FROM PANEL 11/05/2017
+    Platinum Package should not always rely on bank balance and salary. It should also consider
+    features like Residential_Status, Customer_Loyalty etc.
+"""
 
 def allocate_service(ModelAdmin, request, queryset):
     platinum_customers = []
@@ -220,153 +147,49 @@ def allocate_service(ModelAdmin, request, queryset):
 
 
     for customer in queryset:
-
-        # @operable
-        # def getAge():
-        #     age = 0
-        #     #if customer.Age == '60 +':
-        #     if customer.Age == Customer.objects.get(Age = "60 +"):
-        #         age = 0
-        #     #elif customer.Age == '36 - 59':
-        #     elif customer.Age == Customer.objects.get(Age = "36 - 59"):
-        #         age = 1
-        #     else:
-        #         age = 2
-        #     return age
-        #
-        # @operable
-        # def getEducation():
-        #     education = 0
-        #     if customer.Education == Customer.objects.get(Education = "Highschool and below"):
-        #         education = 0
-        #     else:
-        #         education = 1
-        #     return education
-        #
-        # @operable
-        # def getEmployment():
-        #     employment = 0
-        #     if customer.Employment == Customer.objects.get(Employment = "Student"):
-        #         employment = 0
-        #     elif customer.Employment == Customer.objects.get(Employment = "Contract"):
-        #         employment = 1
-        #     else:
-        #         employment = 2
-        #     return employment
-        #
-        # @operable
-        # def getStability():
-        #     stability = 0
-        #     if customer.Employer_Stability == Customer.objects.get(Employer_Stability = "Unstable"):
-        #         stability = 0
-        #     else:
-        #         stability = 1
-        #     return stability
-        #
-        # @operable
-        # def getResidential():
-        #     residential = 0
-        #     if customer.Residential_Status == Customer.objects.get(Residential_Status = "Rented"):
-        #         residential = 0
-        #     else:
-        #         residential = 1
-        #     return residential
-        #
-        # @operable
-        # def getSalary():
-        #     salary = 0
-        #     if customer.Salary == Customer.objects.get(Salary <= 1000):
-        #         salary = 0
-        #     elif customer.Salary == Customer.objects.get(Salary <= 10001 and Salary > 1000):
-        #         salary = 1
-        #     else:
-        #         salary = 2
-        #     return salary
-        #
-        # @operable
-        # def getLoyalty():
-        #     loyalty = 0
-        #     loy = Customer.objects.get(Customer_Loyalty <= 2)
-        #     #if customer.Customer_Loyalty == Customer.objects.get(Customer_Loyalty <= 2):
-        #     if customer.Customer_Loyalty == loy.Customer_Loyalty:
-        #         loyalty = 0
-        #     else:
-        #         loyalty = 1
-        #     return loyalty
-        #
-        # @operable
-        # def getBalance():
-        #     balance = 0
-        #     if customer.Balance == Customer.objects.get(Balance <= 2500):
-        #         balance = 0
-        #     elif customer.Balance == Customer.objects.get(Balance <= 10001 and Balance > 2500):
-        #         balance = 1
-        #     else:
-        #         balance = 2
-        #     return balance
-        #
-        #
-        # def feat_list():
-        #     total = getAge + getEducation + getEmployment + getStability + getResidential + getSalary + getLoyalty + getBalance
-        #     #print('total:'+ total)
-        #     return total
-
-        #customer = [(Customer.objects.values("Gender", "Account_Type", "Age","Education", "Employment", "Employer_Stability", "Residential_Status", "Salary", "Customer_Loyalty", "Balance").get(pk=customer.Customer_ID))]
-        #flist = feat_list()
-        # loyal = getLoyalty
-        # #a = getAge()
-        # print(flist)
-        # print type(flist)
-        # print(loyal)
-        # print type(loyal)
-        #print(a)
-
-        # if flist > 15 and flist <= 21:
-        #     customer.Service_Level = Service.objects.get(service_name = "Platinum Package")
-        #     platinum_customers.append(customer.Name)
-        # elif flist > 11 and flist <= 15 :
-        #         customer.Service_Level = Service.objects.get(service_name = 'Gold Package')
-        #         gold_customers.append(customer.Name)
-        # elif flist > 0 and flist <= 11:
-        #     customer.Service_Level = Service.objects.get(service_name = 'Silver Package')
-        #     silver_customers.append(customer.Name)
-
-
-        # if feat_list() <= 11:
-        #     customer.Service_Level = Service.objects.get(service_name = 'Silver Package')
-        #     silver_customers.append(customer.Name)
-        # #elif feat_list() > 11 and feat_list() <= 15:
-        # elif 11 < feat_list() <= 15:
-        #     # customer.Service_Level = 'Silver Package'
-        #     customer.Service_Level = Service.objects.get(service_name = 'Gold Package')
-        #     gold_customers.append(customer.Name)
-        # elif feat_list() > 15:
-        #     customer.Service_Level = Service.objects.get(service_name = "Platinum Package")
-        #     platinum_customers.append(customer.Name)
-        # else:
-        #     customer.Service_Level = Service.objects.get(service_name = "No Service Package")
-        #     non_customers.append(customer.name)
-
-
-        if   (customer.Salary > 10000 and customer.Customer_Loyalty > 4 and customer.Employer_Stability == 'Stable' and
-                    customer.Residential_Status == 'Owned' or customer.Balance > 10000):
+        if   (5000 <= customer.Salary > 10000 and customer.Customer_Loyalty > 4 and customer.Employer_Stability == 'Stable' or
+                    customer.Residential_Status == 'Owned' or 500 <= customer.Balance > 10000):
                 customer.Service_Level = Service.objects.get(service_name = 'Platinum Package')
                 platinum_customers.append(customer.Name)
 
-        elif (2500 < customer.Salary <= 10000 and customer.Customer_Loyalty < 4 and
-                            2500 < customer.Balance <= 10000 or customer.Employment == 'Contract' and
-                                        customer.Employer_Stability == 'Stable'):
+
+        elif (2500 < customer.Salary <= 6000 and 7 >= customer.Customer_Loyalty > 4 and
+                            2500 < customer.Balance <= 10000  and
+                                            customer.Education == 'Highschool and below' ) :
+                                            #or customer.Education == 'Tertiary and above'
+                                            #or customer.Employment == 'Contract'
             customer.Service_Level = Service.objects.get(service_name = 'Gold Package')
             gold_customers.append(customer.Name)
 
-        elif (customer.Salary <= 100 and customer.Residential_Status == 'Rented' and customer.Employer_Stability == 'Unstable' and
-                customer.Customer_Loyalty < 4 or customer.Balance <= 300):
+            # if (2500 < customer.Salary <= 6000 and 7 >= customer.Customer_Loyalty > 4 and
+            #                     2500 < customer.Balance <= 10000 and
+            #                                 customer.Employer_Stability == 'Stable' and customer.Employment == 'Contract'):
+            #     customer.Service_Level = Service.objects.get(service_name = 'Gold Package')
+            #     gold_customers.append(customer.Name)
+            #
+            # if (2500 < customer.Salary <= 6000 and 7 >= customer.Customer_Loyalty > 4 and
+            #                     2500 < customer.Balance <= 10000 and
+            #                                 customer.Employer_Stability == 'Stable' and customer.Education == 'Tertiary and above' or customer.Employment == 'Contract'):
+            #     customer.Service_Level = Service.objects.get(service_name = 'Gold Package')
+            #     gold_customers.append(customer.Name)
+            #
+            # if (customer.Customer_Loyalty >= 7 and customer.Employer_Stability == 'Stable' and 2500 < customer.Salary <= 6000 ):
+            #      customer.Service_Level = Service.objects.get(service_name = 'Gold Package')
+            #      gold_customers.append(customer.Name)
+
+        elif (customer.Salary <= 100  and customer.Employer_Stability == 'Unstable' and
+                customer.Customer_Loyalty < 4 and customer.Balance <= 300 or customer.Residential_Status == 'Rented' ):
+                # or customer.Employment == 'Contract'
             customer.Service_Level = Service.objects.get(service_name = "No Service Package")
             non_customers.append(customer.Name)
 
         else:
             customer.Service_Level = Service.objects.get(service_name = 'Silver Package')
             silver_customers.append(customer.Name)
+
+        # if (1 <= customer.Customer_Loyalty <= 3 and customer.Salary  ):
+        #     customer.Service_Level = Service.objects.get(service_name = 'Silver Package')
+        #     silver_customers.append(customer.Name)
         customer.save()
 
         if platinum_customers:
@@ -387,8 +210,8 @@ def export_customers(ModelAdmin, request, queryset):
     response = HttpResponse(content_type='text/csv')
     response['Content-Disposition'] = 'attachment; filename="customers.csv"'
     writer = csv.writer(response)
-    writer.writerow(['Customer_ID', 'Name', 'Gender','Age', 'Nationality', 'Address', 'Account_Type', 'Salary', 'Balance', 'Employer_Stability', 'Customer_Loyalty', 'Residential_Status', 'Service_Level'])
-    customers = queryset.values_list('Customer_ID', 'Name', 'Gender','Age', 'Nationality', 'Address', 'Account_Type', 'Salary', 'Balance', 'Employer_Stability', 'Customer_Loyalty', 'Residential_Status', 'Service_Level')
+    writer.writerow(['Customer_ID', 'Name', 'Gender','Age', 'Nationality', 'Education', 'Address', 'Account_Type', 'Employment', 'Salary', 'Balance', 'Employer_Stability', 'Customer_Loyalty', 'Residential_Status', 'Service_Level'])
+    customers = queryset.values_list('Customer_ID', 'Name', 'Gender','Age', 'Nationality', 'Education', 'Address', 'Account_Type', 'Employment', 'Salary', 'Balance', 'Employer_Stability', 'Customer_Loyalty', 'Residential_Status', 'Service_Level')
     for customer in customers:
         writer.writerow(customer)
     return response
--- web/tbank/customers/forms_trial.py
+++ web/tbank/customers/forms_trial.py
@@ -93,3 +93,199 @@ def allocate_service(ModelAdmin, request, queryset):
             message = 'No customer changes made!'
         ModelAdmin.message_user(request, message, level=SUCCESS)
 allocate_service.short_description = 'Allocate Service'
+
+
+# @operable
+# def getAge():
+#     age = 0
+#     #if customer.Age == '60 +':
+#     if customer.Age == Customer.objects.get(Age = "60 +"):
+#         age = 0
+#     #elif customer.Age == '36 - 59':
+#     elif customer.Age == Customer.objects.get(Age = "36 - 59"):
+#         age = 1
+#     else:
+#         age = 2
+#     return age
+#
+# @operable
+# def getEducation():
+#     education = 0
+#     if customer.Education == Customer.objects.get(Education = "Highschool and below"):
+#         education = 0
+#     else:
+#         education = 1
+#     return education
+#
+# @operable
+# def getEmployment():
+#     employment = 0
+#     if customer.Employment == Customer.objects.get(Employment = "Student"):
+#         employment = 0
+#     elif customer.Employment == Customer.objects.get(Employment = "Contract"):
+#         employment = 1
+#     else:
+#         employment = 2
+#     return employment
+#
+# @operable
+# def getStability():
+#     stability = 0
+#     if customer.Employer_Stability == Customer.objects.get(Employer_Stability = "Unstable"):
+#         stability = 0
+#     else:
+#         stability = 1
+#     return stability
+#
+# @operable
+# def getResidential():
+#     residential = 0
+#     if customer.Residential_Status == Customer.objects.get(Residential_Status = "Rented"):
+#         residential = 0
+#     else:
+#         residential = 1
+#     return residential
+#
+# @operable
+# def getSalary():
+#     salary = 0
+#     if customer.Salary == Customer.objects.get(Salary <= 1000):
+#         salary = 0
+#     elif customer.Salary == Customer.objects.get(Salary <= 10001 and Salary > 1000):
+#         salary = 1
+#     else:
+#         salary = 2
+#     return salary
+#
+# @operable
+# def getLoyalty():
+#     loyalty = 0
+#     loy = Customer.objects.get(Customer_Loyalty <= 2)
+#     #if customer.Customer_Loyalty == Customer.objects.get(Customer_Loyalty <= 2):
+#     if customer.Customer_Loyalty == loy.Customer_Loyalty:
+#         loyalty = 0
+#     else:
+#         loyalty = 1
+#     return loyalty
+#
+# @operable
+# def getBalance():
+#     balance = 0
+#     if customer.Balance == Customer.objects.get(Balance <= 2500):
+#         balance = 0
+#     elif customer.Balance == Customer.objects.get(Balance <= 10001 and Balance > 2500):
+#         balance = 1
+#     else:
+#         balance = 2
+#     return balance
+#
+#
+# def feat_list():
+#     total = getAge + getEducation + getEmployment + getStability + getResidential + getSalary + getLoyalty + getBalance
+#     #print('total:'+ total)
+#     return total
+
+#customer = [(Customer.objects.values("Gender", "Account_Type", "Age","Education", "Employment", "Employer_Stability", "Residential_Status", "Salary", "Customer_Loyalty", "Balance").get(pk=customer.Customer_ID))]
+#flist = feat_list()
+# loyal = getLoyalty
+# #a = getAge()
+# print(flist)
+# print type(flist)
+# print(loyal)
+# print type(loyal)
+#print(a)
+
+# if flist > 15 and flist <= 21:
+#     customer.Service_Level = Service.objects.get(service_name = "Platinum Package")
+#     platinum_customers.append(customer.Name)
+# elif flist > 11 and flist <= 15 :
+#         customer.Service_Level = Service.objects.get(service_name = 'Gold Package')
+#         gold_customers.append(customer.Name)
+# elif flist > 0 and flist <= 11:
+#     customer.Service_Level = Service.objects.get(service_name = 'Silver Package')
+#     silver_customers.append(customer.Name)
+
+
+# if feat_list() <= 11:
+#     customer.Service_Level = Service.objects.get(service_name = 'Silver Package')
+#     silver_customers.append(customer.Name)
+# #elif feat_list() > 11 and feat_list() <= 15:
+# elif 11 < feat_list() <= 15:
+#     # customer.Service_Level = 'Silver Package'
+#     customer.Service_Level = Service.objects.get(service_name = 'Gold Package')
+#     gold_customers.append(customer.Name)
+# elif feat_list() > 15:
+#     customer.Service_Level = Service.objects.get(service_name = "Platinum Package")
+#     platinum_customers.append(customer.Name)
+# else:
+#     customer.Service_Level = Service.objects.get(service_name = "No Service Package")
+#     non_customers.append(customer.name)
+
+
+
+#####  PLAN B ALLOCATION
+
+
+# def allocate_service(ModelAdmin, request, queryset):
+#     platinum_customers = []
+#     silver_customers = []
+#     gold_customers = []
+#     non_customers = []
+#     message = ''
+#
+#
+#
+#     for customer in queryset:
+#         qs = Customer.objects.all()
+#         df = qs.to_dataframe(fieldnames=["Age","Education", "Employment", "Employer_Stability", "Residential_Status", "Salary", "Customer_Loyalty", "Balance", "Service_Level"])
+#         #df = read_frame(qs)
+#
+#         df['Age'] = np.where((df['Age']== '60 +'), 0, (np.where((df['Age'] == '36 - 59'), 1, 2)))
+#         df['Education'] = np.where((df['Education']== 'Highschool and below'), 0,  1)
+#         df['Employment'] = np.where((df['Employment']== 'Student'), 0, (np.where((df['Employment'] == 'Contract'), 1, 0)))
+#         df['Employer_Stability'] = np.where((df['Employer_Stability']== 'Unstable'), 0,  1)
+#         df['Residential_Status'] = np.where((df['Residential_Status'] == 'Rented'), 0,  1)
+#         df['Salary'] = np.where(((df['Salary'] >= 150) & (df['Salary'] <= 1000)), 0, (np.where(((df['Salary'] > 1000) & (df['Salary'] <= 10000)), 1, 2)))
+#         df['Customer_Loyalty'] = np.where((df['Customer_Loyalty'] <= 3), 0,  1)
+#         df['Balance'] = np.where(((df['Balance'] >= 150) & (df['Balance'] <= 2500)), 0, (np.where(((df['Balance'] > 2500) & (df['Balance'] <= 10000)), 1, 2)))
+#
+#         added = (df['Age'])+(df['Education'])+(df['Employment'])+(df['Salary'])+(df['Employer_Stability'])+(df['Customer_Loyalty'])+(df['Balance'])+(df['Residential_Status'])
+#
+#         silver = np.where( added <= 11)
+#
+#         gold = np.where(( added > 11) & (added <= 15))
+#
+#         platinum = np.where((added > 15) & (added <= 21))
+#
+#
+#         print (platinum)
+#         print(silver)
+#         print(gold)
+#
+#         if silver:
+#             customer.Service_Level = Service.objects.get(service_name = 'Silver Package')
+#             silver_customers.append(customer.Name)
+#         elif gold:
+#             customer.Service_Level = Service.objects.get(service_name = 'Gold Package')
+#             gold_customers.append(customer.Name)
+#         elif platinum:
+#             customer.Service_Level = Service.objects.get(service_name = 'Platinum Package')
+#             platinum_customers.append(customer.Name)
+#         else:
+#             customer.Service_Level = Service.objects.get(service_name = "No Service Package")
+#             non_customers.append(customer.name)
+#
+#         # else:
+#         #      message = "This customer does not meet the required criteria"
+#         customer.save()
+#
+#         if platinum_customers:
+#             message = 'The following customers are now Platinum Customers: {}'.format(', '.join(platinum_customers))
+#         if silver_customers:
+#             message = 'The following customers are now Silver Customers: {}'.format(', '.join(silver_customers))
+#         if gold_customers:
+#             message = 'The following customers are now Gold Customers: {}'.format(', '.join(gold_customers))
+#         if not platinum_customers and not silver_customers and not gold_customers:
+#             message = 'No customer changes made!'
+#         ModelAdmin.message_user(request, message, level=SUCCESS)
+# allocate_service.short_description = 'Allocate Service'
